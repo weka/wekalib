05619dbeb164aeb3b3eb9cc513256b03cb0b1bbb
Vince Fleming <vince@weka.io>
2021-03-04 17:53:16 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Wekalib',
+    version='1.0.0',
+    packages=['wekalib'],
+    url='https://github.com/weka/wekalib',
+    license='GNU',
+    author='Vincent Fleming',
+    author_email='vince@weka.io',
+    description='Weka API library'
+)
