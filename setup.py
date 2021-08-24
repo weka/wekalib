594e0e29306ae3c41440d191a6399edf46b0d30c
Vince Fleming <vince@weka.io>
2021-08-24 11:25:19 -0400

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Wekalib',
-    version='1.3.2',
+    version='1.3.3',
     author='Vincent Fleming',
     author_email='vince@weka.io',
     description='Weka JRPC API library'
