d967e6a9000ef3be7b76720153fb79a7c011c348
Vince Fleming <46109871+vince-weka@users.noreply.github.com>
2021-10-01 08:09:56 -0400

    Added support for method directory_quota_list

--- wekalib/wekaapi.py
+++ wekalib/wekaapi.py
@@ -150,7 +150,7 @@ class WekaApi():
         words = len(splitmethod)
 
         # does it end in "_list"?
-        if method != "alerts_list":
+        if method != "alerts_list" and method != "directory_quota_list":
             if splitmethod[words - 1] == "list" or method == "filesystems_get_capacity":
                 for key, value_dict in raw_resp.items():
                     newkey = key.split("I")[0].lower() + "_id"
