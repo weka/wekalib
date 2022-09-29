6f4eaf1aed6dfa3654754cb69faeab47503d2423
Vince Fleming <46109871+vince-weka@users.noreply.github.com>
2022-09-29 09:57:56 -0400

    Fix setup.py and allow new response format

--- wekalib/__init__.py
+++ wekalib/__init__.py
@@ -1,4 +1,9 @@
+from . import _version
+__version__ = _version.get_versions()['version']
 
+from logging import getLogger, NullHandler
+log = getLogger('wekaapi')
+log.addHandler(NullHandler())
 
 
 from .wekaapi import WekaApi
